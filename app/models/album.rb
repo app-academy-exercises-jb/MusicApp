81867fe7f1f2621ea6cc7d89a2a86d64ae9d25c6
Jorge Barreto <jrbd93@gmail.com>
2020-01-27 20:53:54 -0500

    fix album's boolean validation

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,6 @@
 class Album < ApplicationRecord
-  validates :title, :year, :band_id, :studio, presence: true
+  validates :title, :year, :band_id, presence: true
+  validates_inclusion_of :studio, in: [true, false]
   
   belongs_to :band
 end
